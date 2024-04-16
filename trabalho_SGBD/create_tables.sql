4fd83ab530dab19ccb161fbbc1abc579ceeb21b3
Paula Caires <62515804+paulacaires@users.noreply.github.com>
2024-04-15 23:47:15 -0300

    Create create_tables.sql

---
+++ trabalho_SGBD/create_tables.sql
@@ -0,0 +1,21 @@
+/* 'anime_id', 'Name', 'Type', 'Episodes', 'Aired', 'Rating' */
+CREATE TABLE anime (
+	
+
+	
+);
+
+/* 'anime_id' e o 'genres' */
+CREATE TABLE anime_genero (
+
+	
+);
+
+CREATE TABLE user_rating (
+	
+)
+	
+/* Mal ID', 'Username', 'Gender', 'Birthday', 'Location' */
+CREATE TABLE mal_user ( /* My Anime List User */
+	
+);
