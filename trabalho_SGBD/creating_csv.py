92277cfe07440831b663c4d3ef9c10fadd67d1d0
Paula Caires <62515804+paulacaires@users.noreply.github.com>
2024-04-15 23:33:09 -0300

    Add files via upload

---
+++ trabalho_SGBD/creating_csv.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+# Carregar os dados do arquivo CSV dos animes
+df = pd.read_csv('../anime_dataset/anime-dataset-2023.csv')
+
+# Em anime, só quero incluir as colunas 'anime_id', 'Name', 'Type', 'Episodes', 'Aired' e 'Rating'
+anime = df[['anime_id', 'Name', 'Type', 'Episodes', 'Aired', 'Rating']]
+
+# Salvar o novo DataFrame em um novo arquivo CSV
+anime.to_csv('anime.csv', index=False)
+
+# Na tabela anime_genres, queremos incluir o 'anime_id' e o 'genres' de forma normalizada
+anime_genero = df.set_index('anime_id')['Genres'].str.split(', ', expand=True).stack().reset_index(level=1, drop=True)
+
+# Renomear a coluna resultante para 'Gênero'
+anime_genero.name = 'Genre'
+
+# Criar um novo DataFrame com IDs e Gêneros
+anime_genero_normalizado = anime_genero.reset_index()
+
+# Salvar o novo DataFrame em um novo arquivo CSV
+anime_genero_normalizado.to_csv('anime_genero.csv', index=False)
+
+# Carregar os dados do arquivo CSV dos usuários
+df = pd.read_csv('../anime_dataset/users-details-2023.csv')
+
+# Em usuários, só quero incluir as colunas 'Mal ID', 'Username', 'Gender', 'Birthday', 'Location'
+users = df[['Mal ID', 'Username', 'Gender', 'Birthday', 'Location']]
+
+# Salvar o dados dos usuários
+users.to_csv('user.csv', index=False)
