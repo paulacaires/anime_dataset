ca581b49e5512b4922eedccc5872d37f0cfc6727
Paula Caires <62515804+paulacaires@users.noreply.github.com>
2024-05-27 11:19:48 -0300

    Update create_tables.sql
    Criação de tabelas feita com sucesso

--- trabalho_SGBD/create_tables.sql
+++ trabalho_SGBD/create_tables.sql
@@ -1,23 +1,33 @@
 /* 'anime_id', 'Name', 'Type', 'Episodes', 'Aired', 'Rating' */
 CREATE TABLE anime (
 	anime_id 	INTEGER PRIMARY KEY NOT NULL,
-	anime_name	VARCHAR(100) NOT NULL,
-	anime_type	VARCHAR(100),
-	aired 		VARCHAR(100),
-	rating 		VARCHAR(100)	
+	anime_name	VARCHAR(500) NOT NULL,
+	anime_type	VARCHAR(255),
+	aired 		VARCHAR(255),
+	score 		FLOAT	
 );
 
 /* 'anime_id' e o 'genres' */
 CREATE TABLE anime_genero (
-
-	
+	anime_id	INTEGER,
+	genre		VARCHAR(255),
+	CONSTRAINT fk_animeID_animeGenero FOREIGN KEY (anime_id) REFERENCES anime(anime_id)		
 );
-
-CREATE TABLE user_rating (
-	
-)
 	
 /* Mal ID', 'Username', 'Gender', 'Birthday', 'Location' */
 CREATE TABLE mal_user ( /* My Anime List User */
-	
+	user_id INTEGER PRIMARY KEY NOT NULL,
+	username VARCHAR(255) NOT NULL,
+	gender VARCHAR(255),
+	birthday VARCHAR(255),
+	location VARCHAR(255)
+);
+
+/* Igual à tabela user_filtered */
+CREATE TABLE user_rating (
+	user_id		INTEGER UNIQUE NOT NULL,
+	anime_id	INTEGER UNIQUE NOT NULL,
+	rating 		FLOAT,
+	CONSTRAINT fk_userID_userRating FOREIGN KEY (user_id) REFERENCES mal_user(user_id),
+	CONSTRAINT fk_animeID_userRating FOREIGN KEY (anime_id) REFERENCES anime(anime_id)
 );
